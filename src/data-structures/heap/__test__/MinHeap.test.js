5e75378280ebab3438d04b56b8ebb823f3c4649a
Oleksii Trekhleb <trehleb@gmail.com>
2018-03-30 08:07:13 +0300

    Add Heap.

--- src/data-structures/heap/__test__/MinHeap.test.js
+++ src/data-structures/heap/__test__/MinHeap.test.js
@@ -34,20 +34,24 @@ describe('MinHeap', () => {
     minHeap.add(5);
     minHeap.add(3);
     minHeap.add(10);
+    minHeap.add(11);
     minHeap.add(1);
 
-    expect(minHeap.toString()).toBe('1,3,10,5');
+    expect(minHeap.toString()).toBe('1,3,10,11,5');
 
     expect(minHeap.poll()).toBe(1);
-    expect(minHeap.toString()).toBe('3,5,10');
+    expect(minHeap.toString()).toBe('3,5,10,11');
 
     expect(minHeap.poll()).toBe(3);
-    expect(minHeap.toString()).toBe('5,10');
+    expect(minHeap.toString()).toBe('5,11,10');
 
     expect(minHeap.poll()).toBe(5);
-    expect(minHeap.toString()).toBe('10');
+    expect(minHeap.toString()).toBe('10,11');
 
     expect(minHeap.poll()).toBe(10);
+    expect(minHeap.toString()).toBe('11');
+
+    expect(minHeap.poll()).toBe(11);
     expect(minHeap.toString()).toBe('');
 
     expect(minHeap.poll()).toBeNull();
