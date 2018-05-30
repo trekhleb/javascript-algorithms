6a87f593e321063600cda98496ee1b8c354fe1a4
Matej Jellus <matej.jellus@itera.no>
2018-05-30 13:34:31 +0200

    Update code to corespond with eslint

--- src/algorithms/uncategorized/n-queens-bitwise/nQueensBitwise.js
+++ src/algorithms/uncategorized/n-queens-bitwise/nQueensBitwise.js
@@ -1,34 +1,33 @@
-export default function(n) {
-  //Keeps track of the # of valid solutions
-  var count = 0;
+export default function (n) {
+  // Keeps track of the # of valid solutions
+  let count = 0;
 
-  //Helps identify valid solutions
-  var done = Math.pow(2,n) - 1;
+  // Helps identify valid solutions
+  const done = (2 ** n) - 1;
 
-  //Checks all possible board configurations
-  var innerRecurse = function(ld, col, rd) {
-
-    //All columns are occupied,
-    //so the solution must be complete
+  // Checks all possible board configurations
+  const innerRecurse = (ld, col, rd) => {
+    // All columns are occupied,
+    // so the solution must be complete
     if (col === done) {
-      count++;
+      count += 1;
       return;
     }
 
-    //Gets a bit sequence with "1"s
-    //whereever there is an open "slot"
-    var poss = ~(ld | rd | col);
+    // Gets a bit sequence with "1"s
+    // whereever there is an open "slot"
+    let poss = ~(ld | rd | col);
 
-    //Loops as long as there is a valid
-    //place to put another queen.
-    while ( poss & done ) {
-      var bit = poss & -poss;
+    // Loops as long as there is a valid
+    // place to put another queen.
+    while (poss & done) {
+      const bit = poss & -poss;
       poss -= bit;
-      innerRecurse((ld|bit)>>1, col|bit, (rd|bit)<<1);
+      innerRecurse((ld | bit) >> 1, col | bit, (rd | bit) << 1);
     }
   };
 
-  innerRecurse(0,0,0);
+  innerRecurse(0, 0, 0);
 
   return count;
-};
+}
