90b56bc3d36a05b67cf10d0ddd237fe03e679a15
Oleksii Trekhleb <trehleb@gmail.com>
2018-06-02 22:11:01 +0300

    Fix issue #37 with AVL tree.

--- src/data-structures/tree/avl-tree/AvlTree.js
+++ src/data-structures/tree/avl-tree/AvlTree.js
@@ -80,6 +80,12 @@ export default class AvlTree extends BinarySearchTree {
     const leftRightNode = leftNode.right;
     leftNode.setRight(null);
 
+    // Preserve leftRightNode's left subtree.
+    if (leftRightNode.left) {
+      leftNode.setRight(leftRightNode.left);
+      leftRightNode.setLeft(null);
+    }
+
     // Attach leftRightNode to the rootNode.
     rootNode.setLeft(leftRightNode);
 
@@ -101,6 +107,11 @@ export default class AvlTree extends BinarySearchTree {
     // Detach left node from rightNode.
     const rightLeftNode = rightNode.left;
     rightNode.setLeft(null);
+
+    if (rightLeftNode.right) {
+      rightNode.setLeft(rightLeftNode.right);
+      rightLeftNode.setRight(null);
+    }
 
     // Attach rightLeftNode to the rootNode.
     rootNode.setRight(rightLeftNode);
