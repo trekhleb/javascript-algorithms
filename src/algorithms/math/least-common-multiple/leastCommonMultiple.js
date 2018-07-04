ddbe8bce9b4e5b38184fd4db2b6c72a54779f357
Bruce Feldman <bfeldman@sunriselabs.com>
2018-07-03 23:23:06 -0400

    Least Common Multiple: Fill undefined value

--- src/algorithms/math/least-common-multiple/leastCommonMultiple.js
+++ src/algorithms/math/least-common-multiple/leastCommonMultiple.js
@@ -7,9 +7,5 @@ import euclideanAlgorithm from '../euclidean-algorithm/euclideanAlgorithm';
  */
 
 export default function leastCommonMultiple(a, b) {
-  if (a === 0 && b === 0) {
-    return 0;
-  }
-
-  return Math.abs(a * b) / euclideanAlgorithm(a, b);
+  return ((a === 0) || (b === 0)) ? 0 : Math.abs(a * b) / euclideanAlgorithm(a, b);
 }
