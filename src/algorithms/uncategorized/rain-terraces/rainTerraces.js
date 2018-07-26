9dd0f66b61382c3c646c6a9e56f5ea7f9c12da15
Keoni Garner <keoni_garner@yahoo.com>
2018-07-26 08:55:18 -0700

    Fixed linting errors

--- src/algorithms/uncategorized/rain-terraces/rainTerraces.js
+++ src/algorithms/uncategorized/rain-terraces/rainTerraces.js
@@ -5,13 +5,13 @@
 
 /*
  * STEPS
- * 1. Find the highest terraces on the left and right side of the elevation map: 
+ * 1. Find the highest terraces on the left and right side of the elevation map:
  * e.g. [0, 2, 4, 3, 1, 2, 4, 0, 8, 7, 0] => (leftMax = 4, rightMax = 8)
  * This is because water will "trail off" the sides of the terraces.
- * 
+ *
  * 2. At this point, we are essentially dealing with a new map: [4, 3, 4, 2, 4, 0, 8].
- * From here, we loop through the map from the left to the right (if leftMax > rightMax, 
- * otherwise we move from right to left), adding water as we go unless we reach a value 
+ * From here, we loop through the map from the left to the right (if leftMax > rightMax,
+ * otherwise we move from right to left), adding water as we go unless we reach a value
  * that is greater than or equal to leftMax || rightMax.
  * e.g. [4, 3, 4, 2, 4, 0, 8]
  *             ^
@@ -20,57 +20,56 @@
  * e.g. [8, 0, 4, 2, 4, 3, 4]
  *                   ^
  * water += rightMax - 3 => water = 1
- * 
+ *
  * 3. Again, we've essentially shortened the map: [4, 2, 4, 0, 8].
  * Now we repeat the above steps on the new array.
- * e.g. 
+ * e.g.
  * Next Iteration:
  * [4, 2, 4, 0, 8]
  *        ^
  * water += leftMax - 2 => water = 3
- * 
+ *
  * Next Iteration:
  * [4, 0, 8]
  *        ^
  * water += leftMax - 0 => water = 7
- * 
+ *
  * return water(7)
  */
 export default function rainTerraces(terraces) {
-
-  let start = 0,
-    end = terraces.length - 1,
-    water = 0,
-    leftMax = 0,
-    rightMax = 0;
+  let start = 0;
+  let end = terraces.length - 1;
+  let water = 0;
+  let leftMax = 0;
+  let rightMax = 0;
 
   while (start < end) {
     // Loop to find left max
     while (start < end && terraces[start] <= terraces[start + 1]) {
-      start++;
+      start += 1;
     }
     leftMax = terraces[start];
 
     // Loop to find right max
     while (end > start && terraces[end] <= terraces[end - 1]) {
-      end--;
+      end -= 1;
     }
     rightMax = terraces[end];
 
     // Determine which direction we need to move in
     if (leftMax < rightMax) {
       // Move from left to right and collect water
-      start++;
+      start += 1;
       while (start < end && terraces[start] <= leftMax) {
         water += leftMax - terraces[start];
-        start++;
+        start += 1;
       }
     } else {
       // Move from left to right and collect water
-      end--;
+      end -= 1;
       while (end > start && terraces[end] <= rightMax) {
         water += rightMax - terraces[end];
-        end--;
+        end -= 1;
       }
     }
   }
