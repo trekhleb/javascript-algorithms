730128f1f5a90cfcbf0b4cf1bb346b7afdd760fe
Keoni Garner <keoni_garner@yahoo.com>
2018-07-26 09:01:48 -0700

    Fixed linting errors

--- src/algorithms/uncategorized/rain-terraces/spec/rainTerraces.test.js
+++ src/algorithms/uncategorized/rain-terraces/spec/rainTerraces.test.js
@@ -1,7 +1,7 @@
 import rainTerraces from '../rainTerraces';
 
-describe('rainTerraces', function () {
-  it('should find the amount of water collected after raining', function () {
+describe('rainTerraces', () => {
+  it('should find the amount of water collected after raining', () => {
     expect(rainTerraces([2, 0, 2])).toBe(2);
     expect(rainTerraces([3, 0, 0, 2, 0, 4])).toBe(10);
     expect(rainTerraces([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])).toBe(6);
