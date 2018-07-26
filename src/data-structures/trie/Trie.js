80e3216609defba5c78763a803642d59be7378be
Oleksii Trekhleb <trehleb@gmail.com>
2018-07-26 18:02:34 +0300

    Add annotations to Trie.

--- src/data-structures/trie/Trie.js
+++ src/data-structures/trie/Trie.js
@@ -1,5 +1,6 @@
 import TrieNode from './TrieNode';
 
+// Character that we will use for trie tree root.
 const HEAD_CHARACTER = '*';
 
 export default class Trie {
@@ -7,15 +8,26 @@ export default class Trie {
     this.head = new TrieNode(HEAD_CHARACTER);
   }
 
+  /**
+   * @param {string} word
+   * @return {Trie}
+   */
   addWord(word) {
     const characters = Array.from(word);
     let currentNode = this.head;
+
     for (let charIndex = 0; charIndex < characters.length; charIndex += 1) {
       const isComplete = charIndex === characters.length - 1;
       currentNode = currentNode.addChild(characters[charIndex], isComplete);
     }
+
+    return this;
   }
 
+  /**
+   * @param {string} word
+   * @return {string[]}
+   */
   suggestNextCharacters(word) {
     const lastCharacter = this.getLastCharacterNode(word);
 
@@ -26,17 +38,27 @@ export default class Trie {
     return lastCharacter.suggestChildren();
   }
 
+  /**
+   * @param {string} word
+   * @return {boolean}
+   */
   doesWordExist(word) {
     return !!this.getLastCharacterNode(word);
   }
 
+  /**
+   * @param {string} word
+   * @return {TrieNode}
+   */
   getLastCharacterNode(word) {
     const characters = Array.from(word);
     let currentNode = this.head;
+
     for (let charIndex = 0; charIndex < characters.length; charIndex += 1) {
       if (!currentNode.hasChild(characters[charIndex])) {
         return null;
       }
+
       currentNode = currentNode.getChild(characters[charIndex]);
     }
 
