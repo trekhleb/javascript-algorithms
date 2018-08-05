5a16d8eaedc335ecbe95a07159a2e8c1853211a4
Victor <romashencovictor@gmail.com>
2018-08-05 10:40:22 -0700

    If statement, not while
    I think it's just more like a typo, it should be a if statement

--- src/data-structures/linked-list/LinkedList.js
+++ src/data-structures/linked-list/LinkedList.js
@@ -67,7 +67,7 @@ export default class LinkedList {
 
     // If the head must be deleted then make next node that is differ
     // from the head to be a new head.
-    while (this.head && this.compare.equal(this.head.value, value)) {
+    if (this.head && this.compare.equal(this.head.value, value)) {
       deletedNode = this.head;
       this.head = this.head.next;
     }
