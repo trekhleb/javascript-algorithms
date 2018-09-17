c69108e7cd42e66725d58a8455dd157c370ef7c2
Vamshi Bachaneboina <vamsisai999@gmail.com>
2018-09-17 15:37:50 +0530

    Update euclideanAlgorithm.js
    Method 2 is easy to evaluate or understand without using recursion stack!

--- src/algorithms/math/euclidean-algorithm/euclideanAlgorithm.js
+++ src/algorithms/math/euclidean-algorithm/euclideanAlgorithm.js
@@ -3,9 +3,23 @@
  * @param {number} originalB
  * @return {number}
  */
+
+/*Method 1: A bit Complex to understand*/
 export default function euclideanAlgorithm(originalA, originalB) {
   const a = Math.abs(originalA);
   const b = Math.abs(originalB);
 
   return (b === 0) ? a : euclideanAlgorithm(b, a % b);
+}
+
+/*Method 2: Easy to evaluate*/
+export default function euclideanAlgorithm2(originalA, originalB) {
+  const a = Math.abs(originalA);
+  const b = Math.abs(originalB);
+  
+  while(a != b){
+    [a,b] = a>b : [a-b, b] : [a, b-a]
+  }
+  
+  return a || b;
 }
