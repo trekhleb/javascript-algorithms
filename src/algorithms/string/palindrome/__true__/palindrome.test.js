0a8c56a35f4231473beda0a423e578295afa34b4
Ocn <ocnly@users.noreply.github.com>
2018-09-20 06:15:39 +0400

    Fix errors

--- src/algorithms/string/palindrome/__true__/palindrome.test.js
+++ src/algorithms/string/palindrome/__true__/palindrome.test.js
@@ -2,7 +2,7 @@ import palindrome from '../palindrome';
 
 describe('palindrome', () => {
   it('should return true if word is a palindrome', () => {
-    expect(palindrome('racecar').toBe(true);
-    expect(palindrome('car').toBe(false);
+    expect(palindrome('racecar')).toBe(true);
+    expect(palindrome('car')).toBe(false);
   }
 }
--- src/algorithms/string/palindrome/palindrome.js
+++ src/algorithms/string/palindrome/palindrome.js
@@ -3,6 +3,6 @@
  * @return {boolean}
  */
 export default function palindrome(string) {
-  reverseString = string.split("").reverse().join("");
+  const reverseString = string.split('').reverse().join('');
   return reverseString === string;
 }
