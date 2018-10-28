e6a159129ad3600c6c304519483e5b6164542d54
Vipul Jain <vupuljain2001@gmail.com>
2018-10-27 21:33:41 -0700

    add another solution

--- src/algorithms/math/factorial/factorialRecursive.js
+++ src/algorithms/math/factorial/factorialRecursive.js
@@ -5,3 +5,9 @@
 export default function factorialRecursive(number) {
   return number > 1 ? number * factorialRecursive(number - 1) : 1;
 }
+
+/* Alternate Solution */
+const factorial = num => {
+    return num == 0 ? num+1 : num * factorial(num-1) 
+}
+console.log(factorial(5)) // 120
