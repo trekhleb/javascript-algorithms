5b7b8197f56e4fe90470bf3670a0e1d7cba26d0f
Sergii Tkachenko <hi@sergii.org>
2019-04-03 00:06:21 -0400

    Full adder: even better comments

--- src/algorithms/math/bits/fullAdder.js
+++ src/algorithms/math/bits/fullAdder.js
@@ -8,26 +8,26 @@ import getBit from './getBit';
  * Inspired by: https://www.youtube.com/watch?v=wvJc9CZcvBc
  *
  * Table(1)
- * INPUT | OUT
- * C A B   C R
- * ----- | --- |
- * 0 0 0 | 0 0 |
- * 0 0 1 | 0 1 |
- * 0 1 0 | 0 1 |
- * 0 1 1 | 1 0 |
- * ----- | --- |
- * 1 0 0 | 0 1 |
- * 1 0 1 | 1 0 |
- * 1 1 0 | 1 0 |
- * 1 1 1 | 1 1 |
- * -------------
+ *  INPUT  | OUT
+ *  C Ai Bi | C Si | Row
+ * -------- | -----| ---
+ *  0  0  0 | 0  0 | 1
+ *  0  0  1 | 0  1 | 2
+ *  0  1  0 | 0  1 | 3
+ *  0  1  1 | 1  0 | 4
+ * -------- | ---- | --
+ *  1  0  0 | 0  1 | 5
+ *  1  0  1 | 1  0 | 6
+ *  1  1  0 | 1  0 | 7
+ *  1  1  1 | 1  1 | 8
+ * ---------------------
  *
  * Legend:
- * First C = Carry in
- * A = Number A
- * B = Number B
- * Second C = Carry out
- * R = Result Bit
+ * INPUT C = Carry in, from the previous less-significant stage
+ * INPUT Ai = ith bit of Number A
+ * INPUT Bi = ith bit of Number B
+ * OUT C = Carry out to the next most-significant stage
+ * OUT Si = Bit Sum, ith least significant bit of the result
  *
  *
  * @param {number} a
@@ -46,20 +46,23 @@ export default function fullAdder(a, b) {
     const bi = getBit(b, i);
     const carryIn = carry;
 
-    // Calculate binary ai + bi without carry
+    // Calculate binary Ai + Bi without carry (half adder)
+    // See Table(1) rows 1 - 4: Si = Ai ^ Bi
     const aiPlusBi = ai ^ bi;
-    // Calculate ith bit of the result by "adding" carryIn to ai + bi
-    // See table(1) row 1 - 4 for carry = 0
-    // See table(1) row 2 - 8 for carry = 1
+
+    // Calculate ith bit of the result by adding the carry bit to Ai + Bi
+    // For Table(1) rows 5 - 8 carryIn = 1: Si = Ai ^ Bi ^ 1, flip the bit
+    // Fpr Table(1) rows 1 - 4 carryIn = 0: Si = Ai ^ Bi ^ 0, a no-op.
     const bitSum = aiPlusBi ^ carryIn;
 
-    // Carry out a bit when at least one of these is true:
-    // 1) table(1) rows 6, 7: carryIn = 1 AND one of ai OR bi is 1
-    // 2) table(1) rows 4, 8: Both ai AND bi are 1
+    // Carry out one to the next most-significant stage
+    // when at least one of these is true:
+    // 1) Table(1) rows 6, 7: one of Ai OR Bi is 1 AND carryIn = 1
+    // 2) Table(1) rows 4, 8: Both Ai AND Bi are 1
     const carryOut = (aiPlusBi & carryIn) | (ai & bi);
     carry = carryOut;
 
-    // Set ith bit of the result to bitSum.
+    // Set ith least significant bit of the result to bitSum.
     result |= bitSum << i;
   }
   return result;
