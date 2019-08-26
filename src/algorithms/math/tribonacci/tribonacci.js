8ee786a5984427b1908d24cdf449ee007a7b0dd5
edumoreira1506 <contato@eduardoem.com.br>
2019-08-26 09:12:16 -0300

    Fixing the spaces identation

--- src/algorithms/math/tribonacci/tribonacci.js
+++ src/algorithms/math/tribonacci/tribonacci.js
@@ -5,18 +5,18 @@
  * @return {number[]}
  */
 export default function tribonacci(n) {
-    const tribSequence = [0];
-    let currentValue = 1;
+  const tribSequence = [0];
+  let currentValue = 1;
   
-    if(n >= 2) {
-        tribSequence.push(currentValue);
-    }
+  if(n >= 2) {
+    tribSequence.push(currentValue);
+  }
     
-    for (let i = 2; i < n; i++) {
-        tribSequence.push(currentValue);
-        currentValue += tribSequence[i - 1] + tribSequence[i - 2];       
-    }
+  for (let i = 2; i < n; i++) {
+    tribSequence.push(currentValue);
+    currentValue += tribSequence[i - 1] + tribSequence[i - 2];       
+  }
   
-    return tribSequence;
+  return tribSequence;
 }
   
--- src/algorithms/math/tribonacci/tribonacciNth.js
+++ src/algorithms/math/tribonacci/tribonacciNth.js
@@ -5,14 +5,14 @@
  * @return {number}
  */
 export default function tribonacciNth(n) {
-    const tribSequence = [0, 0, 1];
-    let currentValue = 1;
+  const tribSequence = [0, 0, 1];
+  let currentValue = 1;
     
-    for (let i = 3; i <= n; i++) {
-        tribSequence.push(currentValue);
-        currentValue += tribSequence[i - 1] + tribSequence[i - 2];       
-    }
+  for (let i = 3; i <= n; i++) {
+    tribSequence.push(currentValue);
+    currentValue += tribSequence[i - 1] + tribSequence[i - 2];       
+  }
   
-    return tribSequence[n];
+  return tribSequence[n];
 }
   
