9dd7b3cd013df73da405d68978a864f08884c02e
Emmanuelveslin <41759142+Emmanuelveslin@users.noreply.github.com>
2019-10-05 23:48:03 +0530

    added counting sort repo

---
+++ countingsort.js
@@ -0,0 +1,22 @@
+let countingSort = (arr, min, max) => {
+    let i = min,
+        j = 0,
+        len = arr.length,
+        count = [];
+    for (i; i <= max; i++) {
+        count[i] = 0;
+    }
+    for (i = 0; i < len; i++) {
+        count[arr[i]] += 1;
+    }
+    for (i = min; i <= max; i++) {
+        while (count[i] > 0) {
+            arr[j] = i;
+            j++;
+            count[i]--;
+        }
+    }
+    return arr;
+};
+
+
