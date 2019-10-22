408b0fc5dfb68a943eb34269aa38119c05bfaf49
Rohith K <41951766+RohithKulothungan@users.noreply.github.com>
2019-10-22 12:42:12 +0530

    Adding indentation fix

--- src/algorithms/search/sentinal-linear-search/sentinalLinearSearch.js
+++ src/algorithms/search/sentinal-linear-search/sentinalLinearSearch.js
@@ -11,27 +11,25 @@ import Comparator from '../../../utils/comparator/Comparator';
 export default function sentinalLinearSearch(array, seekElement, comparatorCallback) {
   const comparator = new Comparator(comparatorCallback);
   const foundIndices = [];
-  var lastEle = array[array.length - 1] // Picking the last element in the array
-  var i = 0;
+  const lastEle = array[array.length - 1]; // Picking the last element in the array
+  const i = 0;
 
-  while (comparator.notEqual(i, array.length-1))
+  while (comparator.notEqual(i, array.length - 1))
   {
     while(comparator.notEqual(array[i], seekElement))
     {
-        i++;
+      i = i + 1;
     }
-    if(comparator.notEqual(i, array.length-1))
+    if(comparator.notEqual(i, array.length - 1))
     {
-        foundIndices.push(i);
-        i++;
+      foundIndices.push(i);
+      i = i + 1;
     }
   }
-    
   array[array.length - 1] = lastEle; // Replacing the last element
 
   if (comparator.equal(array[array.length - 1], seekElement)) {
     foundIndices.push(i);
   }
-  
   return foundIndices;
 }
