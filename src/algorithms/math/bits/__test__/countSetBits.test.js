be185ac9afab48cb3a7b589ac6d9f6c8a3385098
Alexey Onikov <52637988+onicat@users.noreply.github.com>
2020-08-21 08:21:20 +0300

    Fix infinity loop with negative numbers (#502)
    * Update countSetBits.js
    
    * Update countSetBits.test.js

--- src/algorithms/math/bits/__test__/countSetBits.test.js
+++ src/algorithms/math/bits/__test__/countSetBits.test.js
@@ -11,5 +11,9 @@ describe('countSetBits', () => {
     expect(countSetBits(21)).toBe(3);
     expect(countSetBits(255)).toBe(8);
     expect(countSetBits(1023)).toBe(10);
+    expect(countSetBits(-1)).toBe(32);
+    expect(countSetBits(-21)).toBe(30);
+    expect(countSetBits(-255)).toBe(25);
+    expect(countSetBits(-1023)).toBe(23);
   });
 });
