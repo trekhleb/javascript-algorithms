16d6ed8406fc16f43df9c7cb5361eaa4ec81fa27
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2020-12-17 01:48:39 +0330

    Optimize swap

--- src/algorithms/sorting/insertion-sort/InsertionSort.js
+++ src/algorithms/sorting/insertion-sort/InsertionSort.js
@@ -13,25 +13,22 @@ export default class InsertionSort extends Sort {
 
       // Go and check if previous elements and greater then current one.
       // If this is the case then swap that elements.
-      while (
-        array[currentIndex - 1] !== undefined
-        && this.comparator.lessThan(array[currentIndex], array[currentIndex - 1])
+      while (array[currentIndex - 1] !== undefined
+        && this.comparator.lessThan(array[i], array[currentIndex - 1])
       ) {
         // Call visiting callback.
         this.callbacks.visitingCallback(array[currentIndex - 1]);
-
-        // Swap the elements.
-        [
-          array[currentIndex - 1],
-          array[currentIndex],
-        ] = [
-          array[currentIndex],
-          array[currentIndex - 1],
-        ];
-
         // Shift current index left.
         currentIndex -= 1;
       }
+
+      const valueAtIndex = array[i];
+
+      for (let j = i - 1; j >= currentIndex; j -= 1) {
+        array[j + 1] = array[j];
+      }
+
+      array[currentIndex] = valueAtIndex;
     }
 
     return array;
