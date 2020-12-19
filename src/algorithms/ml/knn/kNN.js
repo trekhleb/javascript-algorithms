adda5c53b37c221573480ec6b949f0f424e4034f
Oleksii Trekhleb <trehleb@gmail.com>
2020-12-19 19:26:02 +0100

    Add Euclidean Distance formula (#602)
    * Add Matrices section with basic Matrix operations (multiplication, transposition, etc.)
    
    * Add Euclidean Distance algorithm.

--- src/algorithms/ml/knn/kNN.js
+++ src/algorithms/ml/knn/kNN.js
@@ -1,24 +1,4 @@
 /**
- * Calculates calculate the euclidean distance between 2 vectors.
- *
- * @param {number[]} x1
- * @param {number[]} x2
- * @returns {number}
- */
-function euclideanDistance(x1, x2) {
-  // Checking for errors.
-  if (x1.length !== x2.length) {
-    throw new Error('Inconsistent vector lengths');
-  }
-  // Calculate the euclidean distance between 2 vectors and return.
-  let squaresTotal = 0;
-  for (let i = 0; i < x1.length; i += 1) {
-    squaresTotal += (x1[i] - x2[i]) ** 2;
-  }
-  return Number(Math.sqrt(squaresTotal).toFixed(2));
-}
-
-/**
  * Classifies the point in space based on k-nearest neighbors algorithm.
  *
  * @param {number[][]} dataSet - array of data points, i.e. [[0, 1], [3, 4], [5, 7]]
@@ -27,6 +7,9 @@ function euclideanDistance(x1, x2) {
  * @param {number} k - number of nearest neighbors which will be taken into account (preferably odd)
  * @return {number} - the class of the point
  */
+
+import euclideanDistance from '../../math/euclidean-distance/euclideanDistance';
+
 export default function kNN(
   dataSet,
   labels,
@@ -42,7 +25,7 @@ export default function kNN(
   const distances = [];
   for (let i = 0; i < dataSet.length; i += 1) {
     distances.push({
-      dist: euclideanDistance(dataSet[i], toClassify),
+      dist: euclideanDistance([dataSet[i]], [toClassify]),
       label: labels[i],
     });
   }
