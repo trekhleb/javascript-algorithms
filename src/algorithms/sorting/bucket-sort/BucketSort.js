a704aed150c1ed9cc9ea247a66e75e4dce41910b
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2020-12-20 18:38:39 +0330

    Add comments to BucketSort.js

--- src/algorithms/sorting/bucket-sort/BucketSort.js
+++ src/algorithms/sorting/bucket-sort/BucketSort.js
@@ -2,27 +2,42 @@ import Sort from '../Sort';
 import InsertionSort from '../insertion-sort/InsertionSort';
 
 export default class BucketSort extends Sort {
-  sort(originalArray, originalLowerBound, originalUpperBound) {
+  sort(originalArray, originalLowerBound, originalUpperBound, originalBucketsCount) {
+    // Check list length.
     if (originalArray.length < 2) {
       return [...originalArray];
     }
+
+    // Assign default value to lowerBound and upperBound if required.
     let lowerBound = originalLowerBound;
     let upperBound = originalUpperBound;
     if (lowerBound === undefined || upperBound === undefined) {
       [lowerBound, upperBound] = this.getBounds(originalArray);
     }
+
+    // return list because all values are equal, so it is sorted.
     if (lowerBound === upperBound) {
       return [...originalArray];
     }
+
+    // Assign default value to bucketsCount if required.
+    let bucketCount = originalBucketsCount;
+    if (originalBucketsCount === undefined) {
+      bucketCount = originalArray.length;
+    }
+
+    // Bucketing values.
     const buckets = [];
     for (let i = 0; i < originalArray.length; i += 1) {
-      const bucketIndex = Math.floor(((originalArray[i] - lowerBound) / (upperBound - lowerBound))
-        * originalArray.length);
+      const bucketIndex = Math.floor(((originalArray[i] - lowerBound)
+        / (upperBound - lowerBound)) * (bucketCount - 1));
       if (buckets[bucketIndex] === undefined) {
         buckets[bucketIndex] = [];
       }
       buckets[bucketIndex].push(originalArray[i]);
     }
+
+    // Sort and merge buckets
     let sorted = [];
     const sorter = new InsertionSort();
     for (let i = 0; i < buckets.length; i += 1) {
