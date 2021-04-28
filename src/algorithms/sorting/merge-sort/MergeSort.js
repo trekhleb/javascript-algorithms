0b4d5c36049831e3a84fee140185cf9cb6095879
MedaiP90 <leonardo.cossutta@gmail.com>
2021-04-28 11:16:42 +0200

    Compacted MergeSort code

--- src/algorithms/sorting/merge-sort/MergeSort.js
+++ src/algorithms/sorting/merge-sort/MergeSort.js
@@ -10,17 +10,14 @@ export default class MergeSort extends Sort {
       return originalArray;
     }
 
-    // Split array on two halves.
+    // Find the index in the array middle point.
     const middleIndex = Math.floor(originalArray.length / 2);
-    const leftArray = originalArray.slice(0, middleIndex);
-    const rightArray = originalArray.slice(middleIndex, originalArray.length);
-
-    // Sort two halves of split array
-    const leftSortedArray = this.sort(leftArray);
-    const rightSortedArray = this.sort(rightArray);
 
     // Merge two sorted arrays into one.
-    return this.mergeSortedArrays(leftSortedArray, rightSortedArray);
+    return this.mergeSortedArrays(
+      this.sort(originalArray.slice(0, middleIndex)),
+      this.sort(originalArray.slice(middleIndex, originalArray.length))
+    );
   }
 
   mergeSortedArrays(leftArray, rightArray) {
@@ -31,18 +28,10 @@ export default class MergeSort extends Sort {
     let rightIndex = 0;
 
     while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
-      let minElement = null;
-
       // Find the minimum element between the left and right array.
-      if (this.comparator.lessThanOrEqual(leftArray[leftIndex], rightArray[rightIndex])) {
-        minElement = leftArray[leftIndex];
-        // Increment index pointer to the right
-        leftIndex += 1;
-      } else {
-        minElement = rightArray[rightIndex];
-        // Increment index pointer to the right
-        rightIndex += 1;
-      }
+      const minElement = this.comparator.lessThanOrEqual(leftArray[leftIndex], rightArray[rightIndex])
+        ? leftArray[leftIndex++]
+        : rightArray[rightIndex++];
 
       // Add the minimum element to the sorted array.
       sortedArray.push(minElement);
@@ -53,8 +42,6 @@ export default class MergeSort extends Sort {
 
     // There will be elements remaining from either the left OR the right
     // Concatenate the remaining elements into the sorted array
-    return sortedArray
-      .concat(leftArray.slice(leftIndex))
-      .concat(rightArray.slice(rightIndex));
+    return [...sortedArray, ...leftArray.slice(leftIndex)), ...rightArray.slice(rightIndex))];
   }
 }
