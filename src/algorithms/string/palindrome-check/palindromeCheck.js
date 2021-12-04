b517c245c5d546239cd4727c9b7a21aab333d5dc
liamlylehr <lylehr@terpmail.umd.edu>
2021-12-04 14:31:35 -0500

    adjusted README

--- src/algorithms/string/palindrome-check/palindromeCheck.js
+++ src/algorithms/string/palindrome-check/palindromeCheck.js
@@ -4,12 +4,14 @@
  */
 
 export default function palindromeCheck(string) {
-	let left = 0;
-	let right = string.length-1;
-	while (left < right) {
-		if (string[left] != string[right]) return false;
-		left++;
-		right--;
-	}
-	return true;
+  let left = 0;
+  let right = string.length - 1;
+  while (left < right) {
+    if (string[left] !== string[right]) {
+      return false;
+    }
+    left += 1;
+    right -= 1;
+  }
+  return true;
 }
