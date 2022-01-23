ea28788ed81541dc175d52c2986e9d55d2246dac
liamlylehr <liam@the-lehrs.com>
2022-01-23 15:42:43 -0500

    added palindromeCheck (#806)
    * added readme
    
    * added readme
    
    * adding palindromeCheck
    
    * adjusted README
    
    Co-authored-by: Oleksii Trekhleb <trehleb@gmail.com>

---
+++ src/algorithms/string/palindrome-check/palindromeCheck.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} string
+ * @return {boolean}
+ */
+
+export default function palindromeCheck(string) {
+  let left = 0;
+  let right = string.length - 1;
+  while (left < right) {
+    if (string[left] !== string[right]) {
+      return false;
+    }
+    left += 1;
+    right -= 1;
+  }
+  return true;
+}
