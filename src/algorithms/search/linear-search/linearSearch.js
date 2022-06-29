964e7d462f48d7f64d92706c7ae87014ba3acd61
aryan <64030782+aryan-cs@users.noreply.github.com>
2022-06-29 15:59:49 -0500

    added documentation to explain the search process

--- src/algorithms/search/linear-search/linearSearch.js
+++ src/algorithms/search/linear-search/linearSearch.js
@@ -8,15 +8,24 @@ import Comparator from '../../../utils/comparator/Comparator';
  * @param {function(a, b)} [comparatorCallback]
  * @return {number[]}
  */
+
+                            /* array, item to look for, callback for comparator object */
 export default function linearSearch(array, seekElement, comparatorCallback) {
+//   Previously imported compartor object to assist in checking value
   const comparator = new Comparator(comparatorCallback);
+  
+//   Array of locations (indices) where value can be found
   const foundIndices = [];
 
+//   Loops through array one time and checks each value in the array to the desired value
   array.forEach((element, index) => {
+    
+//     Adds index to array if the values match
     if (comparator.equal(element, seekElement)) {
       foundIndices.push(index);
     }
   });
 
+//   Returns all the locations the desired value can be found in the given array
   return foundIndices;
 }
