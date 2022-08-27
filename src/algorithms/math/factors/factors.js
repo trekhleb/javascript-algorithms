613ce07b45e8a55b3db31118e3e743de24b564a0
zhiwei8 <108912446+zhiwei8@users.noreply.github.com>
2022-08-27 16:44:30 +0800

    Update factors.js(v1)
    update error according the test

--- src/algorithms/math/factors/factors.js
+++ src/algorithms/math/factors/factors.js
@@ -3,19 +3,19 @@
  * Finds the factor of a number.
  *
  */
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
-export function print_factors(x) {
-  console.log("The factors of", x, "are:");
+dafault export function printFactors(x) {
+  console.log('The factors of', x, 'are:');
 
-  //looping 
+  //  looping 
   for (var i = 1, _pj_a = x + 1; i < _pj_a; i += 1) {
     if (x % i === 0) {
       console.log(i);
     }
   }
 }
-//get the input from the user
+//  get the input from the user
 const num = prompt("Enter an positive integer: ");
 
-print_factors(num);
+printFactors(num);
