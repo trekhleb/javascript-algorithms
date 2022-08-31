11aafdc35df7a42f3b6e1083702199322e701bba
zhiwei8 <108912446+zhiwei8@users.noreply.github.com>
2022-08-31 15:26:50 +0800

    Update factors.js
    Update factors.js for calculate the factor of a positive integer that input by the user.

--- src/algorithms/math/factors/factors.js
+++ src/algorithms/math/factors/factors.js
@@ -5,16 +5,14 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 export default function printFactors(x) {
   console.log('The factors of', x, 'are:');
-
-//looping 
+//  looping 
   for (let i = 1; i <= x; i++) {
     if (x % i === 0) {
       console.log(i);
     }
   }
 }
-
-//get the input from the user
+//  get the input from the user
 const num = prompt("Enter a positive integer: ");
 
 printFactors(num);
