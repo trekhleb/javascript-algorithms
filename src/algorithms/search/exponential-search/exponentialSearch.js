f1d7c4b9aa1fa02c94836a1f018d0c4cfa51bc1f
GohJunLe <108907711+GohJunLe@users.noreply.github.com>
2022-09-03 17:23:41 +0800

    Update src/algorithms/search/exponential-search/exponentialSearch.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/algorithms/search/exponential-search/exponentialSearch.js
+++ src/algorithms/search/exponential-search/exponentialSearch.js
@@ -43,7 +43,7 @@ export default function exponentialSearch(sortedArray, seekElement, comparatorCa
   // If element is present at first location itself
   if (sortedArray.length !== 0) {
     if (comparator.equal(sortedArray[0], seekElement))
-      return 0;
+    }
   }
   // Find range for binary search by repeated doubling
     let range = 1;
