85d17f8f64ea387880541319e1631593ec288774
Devesh35 <devesh46singh@gmail.com>
2023-04-02 20:20:57 +0530

    throw-error-for-negative-factorial

--- src/algorithms/math/factorial/factorialRecursive.js
+++ src/algorithms/math/factorial/factorialRecursive.js
@@ -3,5 +3,6 @@
  * @return {number}
  */
 export default function factorialRecursive(number) {
+  if (number < 0) throw new Error(`Factorial of a negative number (${number}) does not exist!`);
   return number > 1 ? number * factorialRecursive(number - 1) : 1;
 }
