5bd5f80c7812c8527163b2c6283c72b54822600a
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-16 18:47:01 +0530

    fixed an error in max function

--- src/algorithms/uncategorized/kadane's algorithm/kadane_algo.js
+++ src/algorithms/uncategorized/kadane's algorithm/kadane_algo.js
@@ -10,8 +10,8 @@ export default function kadane() {
   let maximum_subarray_sum = array[0];
 
   for (let i = 0; i < array.length; i++) {
-    current_sum = max(current_sum + array[i], array[i]);
-    maximum_subarray_sum = max(maximum_subarray_sum, current_sum);
+    current_sum = Math.max(current_sum + array[i], array[i]);
+    maximum_subarray_sum = Math.max(maximum_subarray_sum, current_sum);
   }
 
   return maximum_subarray_sum;
