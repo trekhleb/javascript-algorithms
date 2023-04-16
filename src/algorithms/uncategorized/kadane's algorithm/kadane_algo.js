b51df2bc1201b99d28dc73bb561d75cef750fe3e
sundaram123krishnan <krishnsundaram@gmail.com>
2023-04-16 19:02:15 +0530

    added explanations

--- src/algorithms/uncategorized/kadane's algorithm/kadane_algo.js
+++ src/algorithms/uncategorized/kadane's algorithm/kadane_algo.js
@@ -6,11 +6,25 @@
  */
 
 export default function kadane() {
+
+
+
+  // This algorithm works for both positive and negative values of the array
+
   let current_sum = array[0];
   let maximum_subarray_sum = array[0];
 
   for (let i = 1; i < array.length; i++) {
+    
+    /*
+     * Continue the subarray sum or start a new
+     * subarray sum beginning at the current element.
+    */
+
     current_sum = Math.max(current_sum + array[i], array[i]);
+
+    // Stores the maximum subarray sum at each iteration.
+
     maximum_subarray_sum = Math.max(maximum_subarray_sum, current_sum);
   }
 
