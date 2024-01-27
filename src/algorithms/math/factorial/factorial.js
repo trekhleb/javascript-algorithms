8c9bce09405592d17f0d9cf3c7a3d1a6b9f3fb9d
Hemanth Kumar M <083hemanth@gmail.com>
2024-01-27 23:03:33 +0530

    Update factorial.js
    updated the factorial program to work with Negative Numbers also.

--- src/algorithms/math/factorial/factorial.js
+++ src/algorithms/math/factorial/factorial.js
@@ -3,6 +3,10 @@
  * @return {number}
  */
 export default function factorial(number) {
+  //resulting to a negative number
+  if(number<0)
+    return NaN;
+  
   let result = 1;
 
   for (let i = 2; i <= number; i += 1) {
