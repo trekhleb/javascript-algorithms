476a47e05e25849cf66ae42eeee102cdfeb1569f
shola <michael.o.situ@gmail.com>
2024-08-22 00:11:38 -0700

    fix: update test label to reflect new function results

--- src/algorithms/sets/longest-increasing-subsequence/__test__/dpLongestIncreasingSubsequence.test.js
+++ src/algorithms/sets/longest-increasing-subsequence/__test__/dpLongestIncreasingSubsequence.test.js
@@ -1,7 +1,7 @@
 import dpLongestIncreasingSubsequence from '../dpLongestIncreasingSubsequence';
 
 describe('dpLongestIncreasingSubsequence', () => {
-  it('should find longest increasing subsequence length', () => {
+  it('should find longest increasing subsequence', () => {
     expect(dpLongestIncreasingSubsequence([
       9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
     ])).toStrictEqual([9]);
